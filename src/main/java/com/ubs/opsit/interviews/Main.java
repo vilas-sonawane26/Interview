a6559dee026614df1c75068a40022294bf8375ae
vilas-sonawane26 <39011571+vilas-sonawane26@users.noreply.github.com>
2018-05-06 16:27:10 +0000

    Add files via upload

--- src/main/java/com/ubs/opsit/interviews/Main.java
+++ src/main/java/com/ubs/opsit/interviews/Main.java
@@ -1,10 +1,22 @@
 package com.ubs.opsit.interviews;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
 		String timeStr = "13:17:01";
+		
+		Calendar calendar = GregorianCalendar.getInstance(); 
+		calendar.setTime(new Date());  
+		timeStr = calendar.get(Calendar.HOUR_OF_DAY) + ":" +  
+		          calendar.get(Calendar.MINUTE) + ":" +      
+		          calendar.get(Calendar.SECOND);    
+		
+		System.out.println("time Input: > " + timeStr);
 		
 		TimeConverter timeConverter = new TimeConverterImpl();
 		String time = timeConverter.convertTime(timeStr);
